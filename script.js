faa5e33362bd13615d004d3ccfe36d18f1d01d79
Asia <asia.godula@gmail.com>
2021-11-07 08:48:49 +0100

    Game finished

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ let openDoor2;
 let openDoor3;
 let closedDoorPath = 'https://content.codecademy.com/projects/chore-door/images/closed_door.svg';
 let startButton = document.getElementById('start');
+let currentlyPlaying = true;
+
+const isBot = (door) => {
+    if (door.src === botDoorPath) {
+        return true;
+    } else {
+        return false;
+    };
+};
 
 const isClicked = (door) => {
     if (door.src === closedDoorPath) {
@@ -19,9 +28,11 @@ const isClicked = (door) => {
     };
 };
 
-const playDoor = () => {
+const playDoor = (door) => {
     numClosedDoors--;
     if (numClosedDoors === 0) {
+        gameOver('win');
+    } else if (isBot(door) === true) {
         gameOver();
     };
 };
@@ -44,30 +55,49 @@ const randomChoreDoorGenerator = () => {
 };
 
 doorImage1.onclick = () => {
-    if (!isClicked(doorImage1)) {
+    if (!isClicked(doorImage1) && currentlyPlaying === true) {
         doorImage1.src = openDoor1;
-        playDoor();
+        playDoor(doorImage1);
     };
 };
 
 doorImage2.onclick = () => {
-    if (!isClicked(doorImage2)) {
+    if (!isClicked(doorImage2) && currentlyPlaying === true) {
         doorImage2.src = openDoor2;
-        playDoor();
+        playDoor(doorImage2);
     };
 };
 
 doorImage3.onclick = () => {
-    if (!isClicked(doorImage3)) {
+    if (!isClicked(doorImage3) && currentlyPlaying === true) {
         doorImage3.src = openDoor3;
-        playDoor();
+        playDoor(doorImage3);
     };
+};
+
+startButton.onclick = () => {
+    if (currentlyPlaying === false) {
+        startRound();
+    };
+};
+
+const startRound = () => {
+    doorImage1.src = closedDoorPath;
+    doorImage2.src = closedDoorPath;
+    doorImage3.src = closedDoorPath;
+    numClosedDoors = 3;
+    startButton.innerHTML = 'Good luck!';
+    currentlyPlaying = true;
+    randomChoreDoorGenerator();
 };
 
 const gameOver = (status) => {
-    if () {
-
+    if (status === 'win') {
+        startButton.innerHTML = 'You win! Play again?';
+    } else {
+        startButton.innerHTML = 'Game over! Play again?'
     };
+    currentlyPlaying = false;
 };
 
-randomChoreDoorGenerator();
+startRound();
